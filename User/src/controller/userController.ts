42c5d99c60734c3058de6be1b3ee93bb7e972894
Sunil Salat <sunil.salat@reynasolutions.com>
2023-08-31 17:53:07 +0530

    yaml file changes added

--- User/src/controller/userController.ts
+++ User/src/controller/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import * as IDalUser from "../dataaccess/userService";
+import { mqClient } from "../events/mq/rpc";
+import { PublishMessage } from "../events/publisher/basePublisher";
 
 export const createUser = async (req: Request, res: Response) => {
   const { firstName, lastName, email, phone, dob } = req.body;
@@ -10,6 +12,17 @@ export const createUser = async (req: Request, res: Response) => {
     phone,
     dob,
   });
+
+  if (obj) {
+    const ch = mqClient.channel;
+    PublishMessage(ch, process.env.EXCHANGE_NAME!, "USER_CREATED", {
+      firstName,
+      lastName,
+      email,
+      phone,
+      dob,
+    });
+  }
 
   res.status(201).json({ data: obj, msg: "User created" });
 };
