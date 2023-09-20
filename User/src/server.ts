f27f6b3860a4e385ceec598d3c63cdc7b79706f8
Sunil Salat <sunil.salat@reynasolutions.com>
2023-09-20 17:09:32 +0530

    user service: middlewares added

--- User/src/server.ts
+++ User/src/server.ts
@@ -1,17 +1,19 @@
-import express, { Request, Response } from 'express'
-import connectToDb from './config/connectDb'
-import { loadRoutes } from './routes'
+require("express-async-errors");
+import express, { Request, Response } from "express";
+import connectToDb from "./config/connectDb";
+import { loadRoutes } from "./routes";
+import { ErrorHandlerMiddleware, NotFoundRoute } from "./middlewares";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Welcome to user application ')
-})
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Welcome to user application ");
+});
 
-loadRoutes(app)
-app.use('*', async (req: Request, res: Response) => {
-  res.send('Page not found on user app')
-})
+loadRoutes(app);
 
-export { app }
+app.use(NotFoundRoute);
+app.use(ErrorHandlerMiddleware);
+
+export { app };
