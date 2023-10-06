fa25171f48a7cee94612d7dd3318b217c0ba98f5
Sunil Salat <sunil.salat@reynasolutions.com>
2023-10-06 18:33:20 +0530

    order: message published on successful order placre

--- Order/src/controller/orderController.ts
+++ Order/src/controller/orderController.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from "express";
 import * as IDalOrder from "../dataaccess/orderService";
+import { PublishMessage } from "../events/publisher/basePublisher";
+import { mqClient } from "../events/mq/rpc";
 
 export const createOrder = async (req: Request, res: Response) => {
-  const { data } = req.body;
+    const { data } = req.body;
 
-  const obj = await IDalOrder.createOrder(data);
-  res.status(201).json({ data: obj, msg: "Order created" });
+    const obj = await IDalOrder.createOrder(data);
+
+    if (obj) {
+        const ch = mqClient.channel;
+        PublishMessage(ch, process.env.EXCHANGE_NAME!, "ORDER_CREATED", {
+            orderId: obj._id,
+        });
+    }
+
+    res.status(201).json({ data: obj, msg: "Order created" });
 };
 
 export const getAllOrder = async (req: Request, res: Response) => {
-  const {} = req.body;
-  const obj = await IDalOrder.findMultipleOrderByFilter({});
-  res.status(201).json({ data: obj, msg: "" });
+    const {} = req.body;
+    const obj = await IDalOrder.findMultipleOrderByFilter({});
+    res.status(201).json({ data: obj, msg: "" });
 };
--- Order/src/models/Order.ts
+++ Order/src/models/Order.ts
@@ -1,35 +1,45 @@
 import mongoose, { Schema } from "mongoose";
 
+const OrderItems = new mongoose.Schema({
+    productId: Schema.Types.ObjectId,
+    variantId: Schema.Types.ObjectId,
+    sku: Number,
+    itemName: String,
+    itemPrice: Number,
+    itemQty: Number,
+});
+
 const OrderSchema = new mongoose.Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User id is required"],
-  },
-  orderDate: {
-    type: Date,
-  },
-  address: {
-    type: String,
-    required: [true, "phone is required"],
-  },
-  phone: {
-    type: Number,
-  },
-  status: {
-    type: String,
-    enum: [
-      "CREATED",
-      "APPROVED_By_SELLER",
-      "DISPATCHED",
-      "OUT_FOR_DELIVERY",
-      "DELIVERED",
-    ],
-  },
-  isPaid: {
-    type: Boolean,
-    default: false,
-  },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User id is required"],
+    },
+    orderDate: {
+        type: Date,
+    },
+    address: {
+        type: String,
+        required: [true, "phone is required"],
+    },
+    phone: {
+        type: Number,
+    },
+    status: {
+        type: String,
+        enum: [
+            "CREATED",
+            "APPROVED_By_SELLER",
+            "DISPATCHED",
+            "OUT_FOR_DELIVERY",
+            "DELIVERED",
+        ],
+    },
+    items: [OrderItems],
+    isPaid: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 const Order = mongoose.model("Order", OrderSchema);
